7b42aec17b6bbf861131996383163bc89f8f635a
Foiering Sergey <serjofoi@gmail.com>
2022-11-03 19:29:13 +0200

    Electron ChatApp start project

---
+++ index.js
@@ -0,0 +1,49 @@
+const { app, BrowserWindow, ipcMain, Notification } = require('electron');
+const path = require('path');
+const isDev = !app.isPackaged;
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      worldSafeExecuteJavaScript: true,
+      //* Is a feature that ensures that both, your preload scripts and Electron internal logic run in separate context
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+  win.loadFile('index.html');
+  isDev && win.webContents.openDevTools();
+};
+
+if (isDev) {
+  require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+ipcMain.on('notify', (_, message) => {
+  new Notification({ title: 'Notification', body: message }).show();
+});
+
+ipcMain.on('app-quit', () => {
+  app.quit();
+});
+
+// app.on('window-all-closed', () => {
+//   if (process.platform !== 'darwin') {
+//     app.quit();
+//   }
+// });
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
